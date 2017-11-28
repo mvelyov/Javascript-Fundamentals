fa124524fea26997d62abc92671728cf05de44ac
DESKTOP-I9AC578\ACER <mario.velyov@gmail.com>
2017-11-28 21:49:02 +0200

    Function - generate a random array

---
+++ Functions/Function - generate a random array of elements.js
@@ -0,0 +1,11 @@
+const randomGenerate = (number) => {
+    randomArray = [];
+    for (let i=0; i<number; i+=1) {
+    randomArray[i] = Math.round(( 10 * (Math.random())));
+    }
+    return randomArray;
+}
+
+
+
+
