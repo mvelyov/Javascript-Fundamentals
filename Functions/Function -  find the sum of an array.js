c91d748d02b35c919340cecdeb85d57031efc051
DESKTOP-I9AC578\ACER <mario.velyov@gmail.com>
2017-12-02 17:30:13 +0200

    no message

--- Functions/Function -  find the sum of an array.js
+++
@@ -1,9 +0,0 @@
-const totalSum = (numbers) => {
-    let sum = 0;
-    for (const number of numbers) {
-        sum += number;
-    }
-    return sum;
-};
-
-console.log(totalSum([1, 2, 3, 4, 5]));
--- Functions/Function - generate a random array of elements.js
+++
@@ -1,8 +0,0 @@
-const randomGenerate = (number) => {
-    const randomArray = [];
-    for (let i=0; i<number; i+=1) {
-    randomArray[i] = Math.round(( 10 * (Math.random())));
-    }
-    return randomArray;
-};
-console.log(randomGenerate(5));
