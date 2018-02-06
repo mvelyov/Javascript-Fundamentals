03f794ff5d184b4f3b5a1f6e184278fa961be369
DESKTOP-I9AC578\ACER <mario.velyov@gmail.com>
2018-02-06 19:01:00 +0200

    optimized code and got more points on judge

--- Loops/good-numbers.js
+++ Loops/good-numbers.js
@@ -1,4 +1,4 @@
-const line = '42 142';
+const line = '256 768';
 const input = line.split(' ').map(Number);
 const n = input[0];
 const m = input[1];
@@ -6,11 +6,9 @@ let goodNumbers = m-n+1;
 
 
 for (let start=n; start<=m; start+=1) {
-   const numbers = start.toString().split('').map(Number);
+   const numbers = start.toString().replace(/[0-1]/g, '');
    for (let j=0; j<numbers.length; j+=1) {
-        if (numbers[j] !== 0 &&
-            numbers[j] !== 1 &&
-            start % numbers[j] !== 0) {
+        if (start % numbers[j] !== 0) {
                 goodNumbers -=1;
                 break;
             }
